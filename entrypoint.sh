62c9e0a0bcaf23e358843ff11de0eedd30493d2c
lizhengqiang <237601508@qq.com>
2021-07-07 18:33:58 +0800

    ' to "

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,8 @@ echo $API_PORT
 echo "init"
 
 /gpfs init 
-/gpfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://$HOST_IP:$API_PORT", "http://localhost:3000", "http://127.0.0.1:5001"]'
-/gpfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'
+/gpfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "["http://$HOST_IP:$API_PORT", "http://localhost:3000", "http://127.0.0.1:5001"]"
+/gpfs config --json API.HTTPHeaders.Access-Control-Allow-Methods "["PUT", "POST"]"
 
 echo $MINER_ADDRESS
 echo "start"
