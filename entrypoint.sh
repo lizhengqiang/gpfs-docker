fca90de7842cbff04a27efc6250a2e9dcde2d379
lizhengqiang <237601508@qq.com>
2021-07-07 18:23:02 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-IPFS_PATH=/app/data
+export IPFS_PATH=/app/data
 
 echo $IPFS_PATH
-echo $MINER_ADDRESS
-echo "starting"
+echo $HOST_IP
+echo $API_PORT
+echo "init"
 
+/gpfs init 
+/gpfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://$HOST_IP:$API_PORT", "http://localhost:3000", "http://127.0.0.1:5001"]'
+/gpfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'
+
+echo $MINER_ADDRESS
+echo "start"
 /gpfs daemon --init --miner-address=$MINER_ADDRESS
